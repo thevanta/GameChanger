52abdd652d254f5d9a3bcf38f2d4d68018c37072
Kevin Beltran <kevinbeltranx@gmail.com>
2021-08-29 12:25:52 -0400

    Update CsvReader.cs
    - Reading from csv in editor

--- GameChanger/Assets/_Project/Kevin/Script/CsvReader.cs
+++ GameChanger/Assets/_Project/Kevin/Script/CsvReader.cs
@@ -8,6 +8,13 @@ public class CsvReader : MonoBehaviour
 {
     [SerializeField] private string filePath = @"D:\Box\UnityProjects\GameChanger\Data\8_24_21\example_data_scripts\thighR_FE_1.csv";
     [SerializeField] private List<string> headers;
+    [SerializeField] private List<float> positionX;
+    [SerializeField] private List<float> positionY;
+    [SerializeField] private List<float> positionZ;
+
+    private const int POSITION_X = 6;
+    private const int POSITION_Y = 7;
+    private const int POSITION_Z = 8;
     
 
     private void OnEnable()
@@ -15,6 +22,7 @@ public class CsvReader : MonoBehaviour
         Read();
     }
 
+    [ContextMenu("Read")]
     private void Read()
     {
         StreamReader reader = null;
@@ -24,29 +32,38 @@ public class CsvReader : MonoBehaviour
             reader = new StreamReader(File.OpenRead(filePath));
             List<string> workingList = new List<string>();
             headers = new List<string>();
-            int i = 0;
-            while (!reader.EndOfStream && i < 20)
+
+            var line = reader.ReadLine();
+            var values = line.Split(',');
+            foreach (var item in values)
             {
-                var line = reader.ReadLine();
-                var values = line.Split(',');
-                foreach (var item in values)
+                headers.Add(item);
+            }
+            
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                values = line.Split(',');
+
+                for (var i = 0; i < values.Length; i++)
                 {
-                    if (item.Equals(""))
+                    switch (i)
                     {
-                        print("Empty!");
-                        break;
+                        case POSITION_X: positionX.Add(float.Parse(values[i]));
+                            break;
+                        case POSITION_Y: positionY.Add(float.Parse(values[i]));
+                            break;
+                        case POSITION_Z: positionZ.Add(float.Parse(values[i]));
+                            break;
+                        default:
+                            break;
                     }
-                    
-                    workingList.Add(item);
-                    print(item);
-                    i++;
                 }
                 
-                foreach (var coloumn1 in workingList)
+                foreach (var value in workingList)
                 {
-                    print(coloumn1);
+                    //print(value);
                 }
-                i++;
             }
         }
     }
