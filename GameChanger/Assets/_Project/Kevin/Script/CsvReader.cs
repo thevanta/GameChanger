ef87d4945b341ebd15337a93b2fb0d4854de8e2c
Kevin Beltran <kevinbeltranx@gmail.com>
2021-08-29 13:32:00 -0400

    Created TrackingDataReader
    - Reading position and rotation from data
    - Applying position and rotation, rotation may be incorrect

--- GameChanger/Assets/_Project/Kevin/Script/CsvReader.cs
+++ GameChanger/Assets/_Project/Kevin/Script/CsvReader.cs
@@ -8,19 +8,23 @@ public class CsvReader : MonoBehaviour
 {
     [SerializeField] private string filePath = @"D:\Box\UnityProjects\GameChanger\Data\8_24_21\example_data_scripts\thighR_FE_1.csv";
     [SerializeField] private List<string> headers;
-    [SerializeField] private List<float> positionX;
-    [SerializeField] private List<float> positionY;
-    [SerializeField] private List<float> positionZ;
-
-    private const int POSITION_X = 6;
-    private const int POSITION_Y = 7;
-    private const int POSITION_Z = 8;
     
+    [SerializeField] public List<float> PositionX;
+    [SerializeField] public List<float> PositionY;
+    [SerializeField] public List<float> PositionZ;
+    [SerializeField] public List<float> QuaternionW;
+    [SerializeField] public List<float> QuaternionI;
+    [SerializeField] public List<float> QuaternionJ;
+    [SerializeField] public List<float> QuaternionK;
 
-    private void OnEnable()
-    {
-        Read();
-    }
+    public const int POSITION_X = 6;
+    public const int POSITION_Y = 7;
+    public const int POSITION_Z = 8;
+    public const int QUATERNION_W = 9;
+    public const int QUATERNION_I = 10;
+    public const int QUATERNION_J = 11;
+    public const int QUATERNION_K = 12;
+    
 
     [ContextMenu("Read")]
     private void Read()
@@ -30,7 +34,6 @@ public class CsvReader : MonoBehaviour
         if (File.Exists(filePath))
         {
             reader = new StreamReader(File.OpenRead(filePath));
-            List<string> workingList = new List<string>();
             headers = new List<string>();
 
             var line = reader.ReadLine();
@@ -49,20 +52,23 @@ public class CsvReader : MonoBehaviour
                 {
                     switch (i)
                     {
-                        case POSITION_X: positionX.Add(float.Parse(values[i]));
+                        case POSITION_X: PositionX.Add(float.Parse(values[i]));
                             break;
-                        case POSITION_Y: positionY.Add(float.Parse(values[i]));
+                        case POSITION_Y: PositionY.Add(float.Parse(values[i]));
                             break;
-                        case POSITION_Z: positionZ.Add(float.Parse(values[i]));
+                        case POSITION_Z: PositionZ.Add(float.Parse(values[i]));
+                            break;
+                        case QUATERNION_W: QuaternionW.Add(float.Parse(values[i]));
+                            break;
+                        case QUATERNION_I: QuaternionI.Add(float.Parse(values[i]));
+                            break;
+                        case QUATERNION_J: QuaternionJ.Add(float.Parse(values[i]));
+                            break;
+                        case QUATERNION_K: QuaternionK.Add(float.Parse(values[i]));
                             break;
                         default:
                             break;
                     }
-                }
-                
-                foreach (var value in workingList)
-                {
-                    //print(value);
                 }
             }
         }
---
+++ GameChanger/Assets/_Project/Kevin/Script/TrackingDataReader.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingDataReader : MonoBehaviour
+{
+    [SerializeField] private Transform thisTransform;
+    [SerializeField] private CsvReader csvReader;
+    [SerializeField] private List<float> positionX;
+    [SerializeField] private List<float> positionY;
+    [SerializeField] private List<float> positionZ;
+    [SerializeField] private List<float> quaternionW;
+    [SerializeField] private List<float> quaternionI;
+    [SerializeField] private List<float> quaternionJ;
+    [SerializeField] private List<float> quaternionK;
+
+    private int _currentIndex;
+    private Vector3 _currentPosition;
+    private Quaternion _currentRotation;
+    
+    //Future implementation
+    private enum Tracker
+    {
+        LeftThigh,
+        RightThigh,
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentIndex < positionX.Count)
+        {
+            _currentPosition = new Vector3(
+                positionX[_currentIndex], 
+                positionY[_currentIndex], 
+                positionZ[_currentIndex]);
+            _currentRotation = new Quaternion(
+                quaternionI[_currentIndex], 
+                quaternionJ[_currentIndex],
+                quaternionK[_currentIndex], 
+                quaternionW[_currentIndex]);
+            
+            thisTransform.localPosition = _currentPosition;
+            transform.localRotation = _currentRotation;
+            
+            _currentIndex++;
+        }
+    }
+
+    [ContextMenu("PopulateDependencies")]
+    private void PopulateDependencies()
+    {
+        thisTransform = transform;
+        csvReader = FindObjectOfType<CsvReader>();
+        GetTrackingData();
+    }
+
+    [ContextMenu("GetTrackingData")]
+    private void GetTrackingData()
+    {
+        positionX = new List<float>();
+        positionY = new List<float>();
+        positionZ = new List<float>();
+        quaternionW = new List<float>();
+        quaternionI = new List<float>();
+        quaternionJ = new List<float>();
+        quaternionK = new List<float>();
+        
+        positionX = csvReader.PositionX;
+        positionY = csvReader.PositionY;
+        positionZ = csvReader.PositionZ;
+
+        quaternionI = csvReader.QuaternionI;
+        quaternionJ = csvReader.QuaternionJ;
+        quaternionK = csvReader.QuaternionK;
+        quaternionW = csvReader.QuaternionW;
+        
+        _currentIndex = 0;
+    }
+}
